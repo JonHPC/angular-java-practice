ee1a27e06154b2bb216b27d63577cd9287a9d432
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-11-23 14:11:38 -0800

    Finished backend files

---
+++ employeemanager/src/main/java/com/jonathanhchen/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.jonathanhchen.employeemanager.repo;
+
+import com.jonathanhchen.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+   Optional<Employee> findEmployeeById(Long id);
+}
---
+++ employeemanager/src/main/java/com/jonathanhchen/employeemanager/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.jonathanhchen.employeemanager.service;
+
+import com.jonathanhchen.employeemanager.exception.UserNotFoundException;
+import com.jonathanhchen.employeemanager.model.Employee;
+import com.jonathanhchen.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
